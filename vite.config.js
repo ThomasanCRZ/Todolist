95ef4772bc37674087eb0e41c13fc51711267d62
ThomasanCRZ <Thomas.cruz@outlook.fr>
2025-02-12 18:31:41 +0100

    Fix eror 404

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/", // S'assure que le chemin de base est correct
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    historyApiFallback: true, // Redirige les routes vers index.html
+  },
 })
