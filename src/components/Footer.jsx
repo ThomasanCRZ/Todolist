c16113e202a325fb0cab55687a67b52c201fd3c1
ThomasanCRZ <Thomas.cruz@outlook.fr>
2025-03-05 19:09:08 +0100

    Add link footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,10 @@
 function Footer() {
     const currentYear = new Date().getFullYear();
+    const urlKaiodev = "https://kaiodev.com";
   return (
     <div className="footer">
         <h2>Todolist</h2>
-        <p>Copyright © {currentYear} Kaiodev</p>
+        <p>Copyright © {currentYear} <a href={urlKaiodev}>Kaiodev</a></p>
     </div>
   )
 }
